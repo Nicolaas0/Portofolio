9254f70664ddee0f04b14d977bd8dd6d4a9904c9
Nicolaas0 <nicolerrick@gmail.com>
2020-09-27 04:05:21 +0700

    + Hover + js change fonts

---
+++ script.js
@@ -0,0 +1,19 @@
+function change(){
+document.getElementById('n1').style.fontFamily='Norican', cursive;
+}
+function change2(){
+    document.getElementById('n2').style.fontFamily='lobster', cursive;
+    }
+    function change3(){
+        document.getElementById('n3').style.fontFamily='signika', cursive;
+        }
+        function change4(){
+            document.getElementById('n4').style.fontFamily='bungee', cursive;
+            }
+            function change5(){
+                document.getElementById('n5').style.fontFamily='Shrikhand', cursive;
+                }
+
+function changeimg() {
+    document.getElementById('img1'). src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif";
+}
