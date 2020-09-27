7305ba84ad89d83d2b3a7472302b2c0f61c64019
Nicolaas0 <nicolerrick@gmail.com>
2020-09-27 09:02:54 +0700

    Meraphikan codingan agar enak di lihat

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
 function change(){
-document.getElementById('n1').style.fontFamily='Norican', cursive;
+    document.getElementById('n1').style.fontFamily='Norican', cursive;
 }
+
 function change2(){
     document.getElementById('n2').style.fontFamily='lobster', cursive;
     }
-    function change3(){
-        document.getElementById('n3').style.fontFamily='signika', cursive;
-        }
-        function change4(){
-            document.getElementById('n4').style.fontFamily='bungee', cursive;
-            }
-            function change5(){
-                document.getElementById('n5').style.fontFamily='Shrikhand', cursive;
-                }
 
-function changeimg() {
-    document.getElementById('img1'). src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif";
+function change3(){
+    document.getElementById('n3').style.fontFamily='signika', cursive;
+}
+        
+function change4(){
+    document.getElementById('n4').style.fontFamily='bungee', cursive;
+}
+            
+function change5(){
+    document.getElementById('n5').style.fontFamily='Shrikhand', cursive;
+}
+
+function changegif() {
+    document.getElementById('gif'). src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif";
+}
+
+function changegif2() {
+    document.getElementById('gif2'). src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif";
+}
+
+function changegif3() {
+    document.getElementById('gif3'). src="https://media.giphy.com/media/vFKqnCdLPNOKc/giphy.gif";
 }
